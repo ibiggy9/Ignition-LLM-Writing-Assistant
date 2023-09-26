a3524d29cf09cf51132e08f278319cc4e80c99d5
Ian B <44586707+ibiggy9@users.noreply.github.com>
2023-09-25 20:03:04 -0400

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -10,13 +10,7 @@ import { getDatabase} from 'firebase/database'
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBaW2FyoxrGHsDhNQuFrg1M0j9x1xXu0Bw",
-    authDomain: "ignition-dev-5bf02.firebaseapp.com",
-    projectId: "ignition-dev-5bf02",
-    storageBucket: "ignition-dev-5bf02.appspot.com",
-    messagingSenderId: "260558466476",
-    databaseURL: "https://ignition-dev-5bf02-default-rtdb.firebaseio.com",
-    appId: "1:260558466476:web:5eee23a7d093ac301ade09",
+        <Your Config>
 
   };
   
